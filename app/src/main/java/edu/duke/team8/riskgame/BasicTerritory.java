6f6f343343b85b23721b5a1af104da90ffdb722a
Tiffany <tl330@duke.edu>
2023-03-04 12:09:57 -0500

    add territory player connection

--- app/src/main/java/edu/duke/team8/riskgame/BasicTerritory.java
+++ app/src/main/java/edu/duke/team8/riskgame/BasicTerritory.java
@@ -3,11 +3,14 @@ package edu.duke.team8.riskgame;
 public class BasicTerritory implements Territory {
   //field
   private String name;
+  private Player owner;
   //constructor
-  public BasicTerritory(String name){this.name=name;}
+  public BasicTerritory(String name){this.name=name;this.owner=null;}
+  public BasicTerritory(String name, Player owner){this.name=name;this.owner=owner;}
   //get
-  public String getName(){return name;}
+  @Override
   //equals
+  public String getName(){return name;}
   @Override
   public boolean equals(Object other) {
     if (other != null && other.getClass().equals(getClass())) {
@@ -16,5 +19,19 @@ public class BasicTerritory implements Territory {
     }
     return false;
   }
-  
+
+  @Override
+  public boolean hasOwner(){
+    return !(this.owner==null);
+  }
+
+  @Override
+  public void changeOwner(Player new_owner){
+    this.owner=new_owner;
+  }
+
+  @Override
+  public Player getOwner(){
+    return this.owner;
+  }
 }
--- app/src/test/java/edu/duke/team8/riskgame/BasicTerritoryTest.java
+++ app/src/test/java/edu/duke/team8/riskgame/BasicTerritoryTest.java
@@ -22,4 +22,29 @@ public class BasicTerritoryTest {
     assertTrue(t1.equals(t2));
     assertFalse(t1.equals(s));
   }
+
+  @Test
+  public void test_hasOwner(){
+    String s = "AbcdE";
+    Territory t1 = new BasicTerritory(s);
+    assertFalse(t1.hasOwner());
+
+    Player p=new TextPlayer("red");
+    Territory t2 = new BasicTerritory(s,p);
+    assertTrue(t2.hasOwner());
+    assertEquals(p,t2.getOwner());
+  }
+
+  @Test
+  public void test_changeOwner(){
+    String s = "AbcdE";
+    Player p1=new TextPlayer("red");
+    Territory t = new BasicTerritory(s,p1);
+    assertEquals(p1,t.getOwner());
+    Player p2=new TextPlayer("blue");
+    t.changeOwner(p2);
+    assertEquals(p2,t.getOwner());
+    assertNotEquals(p1, t.getOwner());
+  }
+
 }
