849c13887f70c293a36247776337ced4e06d1b97
yl883 <yl883@duke.edu>
2023-03-05 18:15:17 -0500

    Enabled test from terminal

---
+++ app/src/main/java/edu/duke/team8/riskgame/ClientThread.java
@@ -0,0 +1,49 @@
+package edu.duke.team8.riskgame;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+    private Socket clientSocket;
+    private String color;
+    private String mapInfo;
+    private final BufferedReader input;
+    private final PrintWriter output;
+
+    /**
+     * Constructor of ClientThread
+     * @param clientSocket is the socket of the thread
+     * @param color is color assigned to this client
+     * @param mapInfo is map information
+     * @throws IOException
+     */
+    public ClientThread(Socket clientSocket, String color, String mapInfo) throws IOException {
+        this.clientSocket = clientSocket;
+        this.color = color;
+        this.mapInfo = mapInfo;
+        this.input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.output = new PrintWriter(clientSocket.getOutputStream(), true);
+    }
+    @Override
+    public void run() {
+        try {
+            send();
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Out/Input stream error");
+        }
+    }
+    /**
+     * Send string info to client
+     */
+    public void send() throws IOException {
+        output.println(color);
+        output.println(mapInfo);
+        output.close();
+    }
+
+}
