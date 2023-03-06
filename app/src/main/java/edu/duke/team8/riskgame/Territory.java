cb175025f26957bc4c3c01c6f04864f83d5f0434
Tiffany <tl330@duke.edu>
2023-03-06 16:16:39 -0500

    add territory doc

--- app/src/main/java/edu/duke/team8/riskgame/Territory.java
+++ app/src/main/java/edu/duke/team8/riskgame/Territory.java
@@ -3,61 +3,96 @@ package edu.duke.team8.riskgame;
 import java.util.HashSet;
 
 public interface Territory {
-  //return name
+  /**
+   * @return name
+   */
   public String getName();
 
-  //return adjacent_territory
+  /**
+   * @return adjacent_territory
+   */
   public HashSet<Territory> getAdjacent();
 
-  //return True if equals
-  //return False if not equal
+  /**
+   * @param other
+   * @return True if equals, False if not equal
+   */
   public boolean equals(Object other);
 
-  //return True if Territory have an owner
-  //should always have owner when there is units inside
+  /**
+   * @return True if Territory have an owner
+   * should have owner when there is units inside
+   */
   public boolean hasOwner();
 
-  //change the owner of territory to new_owner
+  /**
+   * change the owner to new_owner
+   * @param new_owner the new owner
+   */
   public void changeOwner(Player new_owner);
 
-  //return True if input is the owner of the territory
+  /**
+   * check if input is the owner
+   * @param owner
+   * @return True if input is the owner, False if otherwise
+   */
   public boolean isOwner(Player owner);
 
-  //add input into adjacent_territory
+  /**
+   * add adjacent into adjacent_territory
+   * @param adjacent
+   */
   public void addAdjacent(Territory adjacent);
 
-  //input territory has the same owner as this
-  //return True if input is adjacent to this
-  //adjacent could mean that it crosses multiple of its own territory to get to this
+  /**
+   * input territory has the same owner as this
+   *
+   * @param adjacent
+   * @return True if input is adjacent to this, False otherwise
+   * adjacent means that it can cross multiple of its own territory to get to this
+   */
   public boolean isAdjacentSelf(Territory adjacent);
 
-  //input territory has different owner as this
-  //return True if input is adjacent to this
-  //adjacent mean that it is directly adjacent to this
+  /**
+   * input territory has different owner as this
+   * @param adjacent
+   * @return True if input is adjacent to this, False otherwise
+   * adjacent mean that it is directly adjacent to this
+   */
   public boolean isAdjacentEnemy(Territory adjacent);
 
-  //add unit_in into units
-  //will modify existing list if units have unit that has the same owner
+  /**
+   * add unit_in into units
+   * will modify existing list if units have unit that has the same owner
+   * @param unit_in
+   */
   public void moveIn(Unit unit_in);
 
-  //pass in any declared unit
-  //will check if the units list have unit that has same owner
-  //return True if sucessfully moved out the unit
-  //return False if the unit number is not big enough for unit_out to be moved out
+  /**
+   * pass in any declared unit
+   * will check if the units list have unit that has same owner and move out the amount
+   * @param unit_out
+   * @return True if sucessfully moved out the unit, False otherwise
+   */
   public boolean tryMoveOut(Unit unit_out);
 
-  //resolve the condition when the units list have a size > 1
-  //the one with a larger roll number wins
-  //if 2 units: defender wins when there is a tie
-  //if >2 units: the left one wins when there is a tie
-  //units will be left with only one unit, which will become the owner
+  /**
+   * resolve the condition when the units list have a size > 1
+   * the one with a larger roll number wins
+   * if 2 units: defender wins when there is a tie
+   * if >2 units: the left one wins when there is a tie
+   * units will be left with only one unit, which will become the owner
+   */
   public void attack();
 
-  //get amount of the nth unit in units
+  /**
+   * @param n
+   * @return amount of the nth unit in units
+   */
   public int getUnitAmount(int n);
 
-  //get the size of units
-  //for testing 
+  /**
+   * @return size of units
+   */
   public int getUnitsSize();
 }
-
