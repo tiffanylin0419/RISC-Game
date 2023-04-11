a82bb74f2fc17457788d82dd7909700aa7c9dd46
shuyan <ss1316@duke.edu>
2023-04-11 12:50:50 -0400

    add UpgradeAction

---
+++ gradle-skeleton/shared/src/main/java/edu/duke/ece651/team8/shared/UpgradeAction.java
@@ -0,0 +1,12 @@
+package edu.duke.ece651.team8.shared;
+
+public class UpgradeAction extends BasicAction {
+    public UpgradeAction(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void doAction() {
+
+    }
+}
