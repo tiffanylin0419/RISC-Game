b77c353db890c3847d14a8cb2b6051c2ab33bbdd
ys386 <ys386@duke.edu>
2023-04-12 22:52:26 -0400

    fix research bug

--- gradle-skeleton/shared/src/test/java/edu/duke/ece651/team8/shared/LevelRuleCheckerTest.java
+++ gradle-skeleton/shared/src/test/java/edu/duke/ece651/team8/shared/LevelRuleCheckerTest.java
@@ -14,7 +14,7 @@ class LevelRuleCheckerTest {
         LevelRuleChecker lrc = new LevelRuleChecker(null, 6);
         assertEquals("Your level must be positive Integer!",lrc.checkMyRule(as));
         p.setLevel(6);
-        assertEquals("Research action is valid since your level can not be greater than the max level: 6!",lrc.checkMyRule(as));
+        assertEquals("Research action is invalid since your level can not be greater than the max level: 6!",lrc.checkMyRule(as));
         p.setLevel(1);
         assertNull(lrc.checkMyRule(as));
     }
