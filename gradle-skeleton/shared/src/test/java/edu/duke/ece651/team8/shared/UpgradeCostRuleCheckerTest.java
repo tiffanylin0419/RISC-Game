2bb50363a1f921db4d2b1bf14dc5f72ce4084fc1
shuyan <ss1316@duke.edu>
2023-04-13 11:42:49 -0400

    add UpgradeUnitRuleChecker test

--- gradle-skeleton/shared/src/test/java/edu/duke/ece651/team8/shared/UpgradeCostRuleCheckerTest.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.ece651.team8.shared;
-import org.junit.jupiter.api.Test;
-
-public class UpgradeCostRuleCheckerTest {
-    @Test
-    public void testUpgradeCostRuleChecker() {
-
-    }
-}
