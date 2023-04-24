0a9d3cf4d19bf9cca7e2c1cd873257be5e40af38
Tiffany <tl330@duke.edu>
2023-04-24 11:47:33 -0400

    grey can see past info

--- gradle-skeleton/shared/src/main/java/edu/duke/ece651/team8/shared/MapGuiView.java
+++ gradle-skeleton/shared/src/main/java/edu/duke/ece651/team8/shared/MapGuiView.java
@@ -77,13 +77,15 @@ public class MapGuiView implements View {
         //grey
         HashSet<Territory> seen_territories2=new HashSet<>();
         for(Territory t: theMap.getTerritories()){
-            if(player.seen_territories.contains(t.getName())){
+            if(player.seen_territories.containsKey(t.getName())){
                 seen_territories2.add(t);
             }
         }
         for(Territory t: seen_territories2){
             if(!checked_territories.contains(t)){
-                displayGreyTerritoryInfo(sb,t);
+                String tname=t.getName();
+                String info=player.seen_territories.get(tname);
+                displayGreyTerritoryInfo(sb,tname,info);
             }
         }
         checked_territories.addAll(seen_territories2);
@@ -92,10 +94,6 @@ public class MapGuiView implements View {
             if(!checked_territories.contains(t)){
                 displayBlackTerritoryInfo(sb,t);
             }
-        }
-        //add checked to seen
-        for(Territory t: checked_territories){
-            player.seen_territories.add(t.getName());
         }
         sb.append("}\n}");
         return sb.toString();
@@ -114,19 +112,23 @@ public class MapGuiView implements View {
     }
 
 
-    public void displayPlayerUnitInfo(StringBuilder sb, Territory t, Player player) {
-        sb.append("    \"army\":\"");
+    public String displayPlayerUnitInfo(StringBuilder sb, Territory t, Player player) {
+        StringBuilder sb2 = new StringBuilder("");
+        sb2.append("    \"army\":\"");
         for(int i=0;i<=6;i++){
-            sb.append("L"+i+": "+t.getOwnerUnitLevelAmount(i)+"\\n");
+            sb2.append("L"+i+": "+t.getOwnerUnitLevelAmount(i)+"\\n");
         }
-        sb.append("My Spy: "+t.getSpyAmount(player)+"\\n");
+        sb2.append("My Spy: "+t.getSpyAmount(player)+"\\n");
         ResourceTerritory tt=(ResourceTerritory) t;
-        sb.append("Food: "+tt.getAddFood()+"\\n");
-        sb.append("Tech: "+tt.getAddTech()+"\\n");
+        sb2.append("Food: "+tt.getAddFood()+"\\n");
+        sb2.append("Tech: "+tt.getAddTech()+"\\n");
         if(tt.isCloaking()){
-            sb.append("Is cloaked\\n");
+            sb2.append("Is cloaked\\n");
         }
-        sb.append("\"\n");
+        sb2.append("\"\n");
+        String ret=sb2.toString();
+        sb.append(sb2);
+        return ret;
     }
 
     public void displayTerritoryInfo(StringBuilder sb, Territory t) {
@@ -139,14 +141,15 @@ public class MapGuiView implements View {
     public void displayPlayerTerritoryInfo(StringBuilder sb, Territory t, Player player) {
         sb.append("  \"").append(t.getName()).append("\":{\n");
         sb.append("    \"color\":\"").append(t.getOwner().getColor()).append("\",\n");
-        displayPlayerUnitInfo(sb,t, player);
+        String info=displayPlayerUnitInfo(sb,t, player);
         sb.append("  },\n");
+        player.seen_territories.put(t.getName(),info);
     }
 
-    public void displayGreyTerritoryInfo(StringBuilder sb, Territory t) {
-        sb.append("  \"").append(t.getName()).append("\":{\n");
+    public void displayGreyTerritoryInfo(StringBuilder sb, String tname,String info) {
+        sb.append("  \"").append(tname).append("\":{\n");
         sb.append("    \"color\":\"").append("Grey").append("\",\n");
-        sb.append("    \"army\":\"Do not have info\"");
+        sb.append(info);
         sb.append("  },\n");
     }
 
