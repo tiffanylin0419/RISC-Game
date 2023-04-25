0d967d70c31eadaa2378f60f0b3cd15195308cde
shuyan <ss1316@duke.edu>
2023-04-25 16:19:33 -0400

    test

--- gradle-skeleton/shared/src/main/java/edu/duke/ece651/team8/shared/ResourceTerritory.java
+++ gradle-skeleton/shared/src/main/java/edu/duke/ece651/team8/shared/ResourceTerritory.java
@@ -86,11 +86,11 @@ public class ResourceTerritory extends BasicTerritory {
                 units.remove(0);
             }
         }
-        for (Army army : this.spyArmies) {
-            List<Unit> units = army.getList();
-            while (!units.isEmpty()) {
-                units.remove(0);
-            }
-        }
+//        for (Army army : this.spyArmies) {
+//            List<Unit> units = army.getList();
+//            while (!units.isEmpty()) {
+//                units.remove(0);
+//            }
+//        }
     }
 }
