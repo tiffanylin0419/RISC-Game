5c6160ad56c671b1e2fc71fb712d764f1a4c0dd3
shuyan <ss1316@duke.edu>
2023-04-28 17:45:45 -0400

    add Spy testcases

---
+++ gradle-skeleton/shared/src/test/java/edu/duke/ece651/team8/shared/SpyMoveActionTest.java
@@ -0,0 +1,79 @@
+package edu.duke.ece651.team8.shared;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class SpyMoveActionTest {
+    @Test
+    public void testIsValidPath() {
+        V1MapFactory factory = new V1MapFactory();
+        Game1Map map = factory.createMap(4);
+        ArrayList<Player> players =factory.createPlayers(4,map);
+
+        ArrayList<Territory> territories=map.getTerritories();
+        Territory s=territories.get(0);
+        Territory d=territories.get(7);
+        s.moveIn(new BasicArmy(4, players.get(0)));
+        d.moveIn(new BasicArmy(5, players.get(1)));
+
+        MovableAction action1=new MoveAction(players.get(0),"a1","b2",3,map);
+        assertTrue(action1.isValidSource());
+
+        MovableAction action2=new MoveAction(players.get(1),"a1","b2",3,map);
+        assertFalse(action2.isValidSource());
+
+        MovableAction action3=new MoveAction(players.get(1),"a1","b2",3,map);
+        assertTrue(action3.isValidDestination());
+
+        MovableAction action4=new MoveAction(players.get(0),"a1","b2",3,map);
+        assertFalse(action4.isValidDestination());
+        assertFalse(action4.isValidPath());
+
+
+        MovableAction action5=new MoveAction(players.get(0),"a2","a3",3,map);
+        action5.getSource().moveIn(new BasicArmy(4, players.get(0)));
+        action5.getDestination().moveIn(new BasicArmy(6, players.get(0)));
+        assertTrue(action5.isValidPath());
+    }
+
+    private ArrayList<Territory> createTerritories() {
+
+        Territory t1 = new ResourceTerritory("a");
+        Territory t2 = new ResourceTerritory("b");
+        Territory t3 = new ResourceTerritory("c");
+        Territory t4 = new ResourceTerritory("d");
+        Player player = new Player("Green");
+        Player player2 = new Player("Red");
+
+        t1.setOwner(player);
+        t2.setOwner(player);
+        t3.setOwner(player);
+        t4.setOwner(player2);
+
+        t1.addAdjacent(t2);
+        t1.addAdjacent(t3);
+        t2.addAdjacent(t3);
+        t1.addAdjacent(t4);
+        t3.addAdjacent(t4);
+
+        t1.setDistance(t2, 3);
+        t2.setDistance(t1, 3);
+        t1.setDistance(t3, 7);
+        t3.setDistance(t1, 7);
+        t2.setDistance(t3, 3);
+        t3.setDistance(t2, 3);
+        t1.setDistance(t4, 2);
+        t4.setDistance(t1, 2);
+        t3.setDistance(t4, 2);
+        t4.setDistance(t3, 2);
+
+        ArrayList<Territory> testMap = new ArrayList<>();
+        testMap.add(t1);
+        testMap.add(t2);
+        testMap.add(t3);
+        testMap.add(t4);
+        return testMap;
+    }
+}
